34c5bafb3aae367613e76902a33412e0b1848c3b
ajkapala21 <ajkapala@hotmail.com>
2025-11-30 16:32:27 -0500

    map size addition

--- scheds/c/scx_rand.bpf.c
+++ scheds/c/scx_rand.bpf.c
@@ -113,7 +113,7 @@ static long sample_cb(u64 idx, struct random_sample_ctx *rand_cxt)
     }
 
     // Optional early exit if time exceeded:
-    if (bpf_ktime_get_ns() - s->start_ns >= s->window_ns){
+    if (bpf_ktime_get_ns() - s->start_ns >= s->window_ns || idx >= map_size){
         //bpf_printk("Exited because of time: %llu\n", idx);
         return 1; // bpf_loop will stop early if callback returns 1
     }
