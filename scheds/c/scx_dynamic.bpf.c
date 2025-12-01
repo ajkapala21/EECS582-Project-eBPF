2003978454effe6a2d7f787d89edf6ca32ec36fa
anthony <ajkapala@hotmail.com>
2025-12-01 16:18:58 -0500

    compiles correctly

--- scheds/c/scx_dynamic.bpf.c
+++ scheds/c/scx_dynamic.bpf.c
@@ -236,8 +236,8 @@ void BPF_STRUCT_OPS(dynamic_running, struct task_struct *p)
 void BPF_STRUCT_OPS(dynamic_stopping, struct task_struct *p, bool runnable)
 {
 	p->scx.dsq_vtime += (SCX_SLICE_DFL - p->scx.slice) * 100 / p->scx.weight;
-    float a = 0.85;
-    avg_slice_used = avg_slice_used * a + (SCX_SLICE_DFL - p->scx.slice) * (1 - a);
+    u64 a = 85;
+    avg_slice_used = avg_slice_used * (a / 100) + (SCX_SLICE_DFL - p->scx.slice) * ((100 - a) / 100);
     bpf_printk("Avg slice used in nanoseconds: %llu\n", avg_slice_used);
 }
 
