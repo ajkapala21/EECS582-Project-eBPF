18539c7b826ad4e036dbf058a9b0e990d345661c
ajkapala21 <ajkapala@hotmail.com>
2025-12-03 16:33:18 -0500

    rand2 percpu

--- scheds/c/scx_rand2.bpf.c
+++ scheds/c/scx_rand2.bpf.c
@@ -127,8 +127,9 @@ void BPF_STRUCT_OPS(rand2_enqueue, struct task_struct *p, u64 enq_flags)
     ti->vruntime = vtime;
     ti->pid = pid;
     ti->valid = valid;
+    context->size++;
 
-    bpf_spin_unlock(&context->unlock);
+    bpf_spin_unlock(&context->lock);
 }
 
 static long sample_cb(u64 idx, struct random_sample_ctx *rand_cxt)
@@ -137,9 +138,16 @@ static long sample_cb(u64 idx, struct random_sample_ctx *rand_cxt)
 
     // use bpf_get_prandom_u32() inside callback
     u32 r = bpf_get_prandom_u32();
-    bpf_spin_lock(&map_lock);
-    u32 key = r % map_size;
-    bpf_spin_unlock(&map_lock);
+
+    int cpu = bpf_get_smp_processor_id();
+    struct map_ctx *context = bpf_map_lookup_percpu_elem(map_size, &zero, cpu);
+    if(!context){
+        bpf_printk("NO CONTEXT FOUND\n");
+        return;
+    }
+    u64 size = context->size;
+    u32 key = r % size;
+
     struct task_ctx *ti = bpf_map_lookup_elem(&task_map, &key);
     if (!ti || !ti->valid) return 0; // continue
 
@@ -149,23 +157,112 @@ static long sample_cb(u64 idx, struct random_sample_ctx *rand_cxt)
     }
 
     // Optional early exit if time exceeded:
-    if (bpf_ktime_get_ns() - s->start_ns >= s->window_ns || idx >= map_size){
+    if (bpf_ktime_get_ns() - s->start_ns >= s->window_ns || idx >= size){
         //bpf_printk("Exited because of time: %llu\n", idx);
         return 1; // bpf_loop will stop early if callback returns 1
     }
-
-    // may want to also add logic to cancel if you have sampled more than the size map_size
-    // currently with the static window of 500ns it samples around 15 (on my pc)
-    // this is overkill if the map_size is small, which it often will be, although maybe not
-    // for benchmarks
         
     return 0; // continue
 }
 
 void BPF_STRUCT_OPS(rand2_dispatch, s32 cpu, struct task_struct *prev)
 {
+    struct random_sample_ctx s = {
+        .start_ns = bpf_ktime_get_ns(),
+        .window_ns = SAMPLE_WINDOW_NS,
+        .best_vtime = (u64)-1,
+        .best_key = -1,
+    };
 
+    int cpu = bpf_get_smp_processor_id();
+    struct map_ctx *context = bpf_map_lookup_percpu_elem(map_size, &zero, cpu);
+    if(!context){
+        bpf_printk("NO CONTEXT FOUND\n");
+        return;
+    }
+    u64 size = context->size;
 
+    if(size > 1){
+        long ret = bpf_loop(SAMPLE_COUNT, sample_cb, &s, 0);
+        
+        // dispatch
+        if (s.best_key >= 0) {
+            //bpf_printk("Key Found\n");
+            struct task_ctx *ti_dis = bpf_map_lookup_elem(&task_map, &s.best_key);
+            if (!ti_dis) {
+                //bpf_printk("TI_DIS null\n");
+                return; 
+            }
+            bpf_spin_lock(&context->lock);
+            u32 key = context->size - 1;
+            bpf_spin_unlock(&context->lock);
+            struct task_ctx *ti_last = bpf_map_lookup_elem(&task_map, &key);
+            if (!ti_last){
+                //bpf_printk("TI_LAST null\n");
+                return;
+            }
+            //invalidate first to ensure only one cpu can dispatch this task
+            bpf_spin_lock(&context->lock);
+            if(!ti_dis->valid || !ti_last->valid || key != context->size - 1){
+                bpf_spin_unlock(&context->lock);
+                //bpf_printk("TI_DIS OR TI_LAST INVALID OR RACE DETECTED\n");
+                return;
+            }
+            // invalidate last task in array and decrement map size
+            context->size--;
+            ti_last->valid = false;
+
+            u32 pid = ti_dis->pid;
+
+            // then move that tasks info to the index of our one about to be dispatched
+            ti_dis->pid = ti_last->pid;
+            ti_dis->vruntime = ti_last->vruntime;
+            bpf_spin_unlock(&context->lock);
+            //convert pid to task struct and dispatch that
+            struct task_struct *task = bpf_task_from_pid(pid);
+            if (!task){
+                //bpf_printk("task struct null\n");
+                return;
+            }
+
+            scx_bpf_dsq_insert(task, SHARED_DSQ, SCX_SLICE_DFL, 0);
+            bpf_task_release(task);
+            //bpf_printk("Successful Dispatch\n");
+            stat_inc(2);
+        }
+    }
+    if(size == 1){
+        u32 key = 0;
+        struct task_ctx *ti = bpf_map_lookup_elem(&task_map, &key);
+        if (!ti){
+            //bpf_printk("TI null\n");
+            return;
+        }
+        //invalidate first to ensure only one cpu can dispatch this task
+        bpf_spin_lock(&context->lock);
+        if(!ti->valid || context->size != 1){
+            bpf_spin_unlock(&context->lock);
+            //bpf_printk("TI INVALID OR RACE DETECTED\n");
+            return;
+        }
+        // invalidate the task in array and decrement map size
+        context->size--;
+        ti->valid = false;
+        u32 pid = ti->pid;
+
+        bpf_spin_unlock(&context->lock);
+
+        struct task_struct *task = bpf_task_from_pid(pid);
+        if (!task){
+            //bpf_printk("task struct null\n");
+            return;
+        }
+        scx_bpf_dsq_insert(task, SHARED_DSQ, SCX_SLICE_DFL, 0);
+        bpf_task_release(task);
+        //bpf_printk("Successful Dispatch\n");
+        stat_inc(2);
+    }
+    scx_bpf_dsq_move_to_local(SHARED_DSQ);
 }
 
 void BPF_STRUCT_OPS(rand2_running, struct task_struct *p)
