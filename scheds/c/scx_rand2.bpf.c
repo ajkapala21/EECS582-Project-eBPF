c22727f28efd76a3097ff9c5500e33eb5706b936
ajkapala21 <ajkapala@hotmail.com>
2025-12-03 15:34:11 -0500

    remove rand2 from makefile

--- scheds/c/scx_rand2.bpf.c
+++ scheds/c/scx_rand2.bpf.c
@@ -12,10 +12,17 @@ char _license[] SEC("license") = "GPL";
 #define SAMPLE_COUNT 500
 
 static u64 vtime_now;
-static u32 map_size = 0;
+static index = 0;
+
 
 UEI_DEFINE(uei);
-#define SHARED_DSQ 0
+
+struct random_sample_ctx {
+    u64 start_ns;
+    u64 window_ns;
+    u64 best_vtime;
+    int  best_key;
+};
 
 struct task_ctx {
     u32 pid;
@@ -23,21 +30,35 @@ struct task_ctx {
     bool valid;
 };
 
-private(rand) struct bpf_spin_lock map_lock;
+struct map_ctx {
+    u64 size;
+    struct bpf_spin_lock lock;
+};
+
+//int cpu = bpf_get_smp_processor_id();
+//int nr = scx_bpf_nr_cpu_ids();
+//bpf_map_lookup_percpu_elem(map, &key, cpu_id);
 
 struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
     __uint(max_entries, MAX_TASKS);
     __type(key, u32);
     __type(value, struct task_ctx);
 } task_map SEC(".maps");
 
 struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, struct map_ctx);
+} map_size SEC(".maps");
+
+struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __uint(max_entries, 1);
     __type(key, u32);
-    __type(value, u64);
-} map_info SEC(".maps");
+    __type(value, u32);
+} cpu_rr_idx SEC(".maps");
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
@@ -45,6 +66,8 @@ struct {
 	__uint(value_size, sizeof(u64));
 	__uint(max_entries, 3);
 } stats SEC(".maps");
+
+private(rand) struct bpf_spin_lock cpu_lock;
 
 static void stat_inc(u32 idx)
 {
@@ -71,29 +94,78 @@ void BPF_STRUCT_OPS(rand2_enqueue, struct task_struct *p, u64 enq_flags)
 {
 	stat_inc(1);	/* count global queueing */
 
-    scx_bpf_random_enqueue(&task_map, &map_info, p, &vtime_now);
+    u64 vtime = p->scx.dsq_vtime;
+    u32 pid = p->pid;
 
-    //bpf_printk("Enqueue: map_size = %llu\n", map_size);
+    if (time_before(vtime, vtime_now - SCX_SLICE_DFL))
+        vtime = vtime_now - SCX_SLICE_DFL;
+
+    bpf_spin_lock(&cpu_lock);
+    u32 cpu_id = index % scx_bpf_nr_cpu_ids();
+    index = (index + 1) % scx_bpf_nr_cpu_ids();
+    bpf_spin_unlock(&cpu_lock);
+
+    u32 zero = 0;
+    struct map_ctx *context = bpf_map_lookup_percpu_elem(map_size, &zero, cpu_id);
+    if(!context){
+        bpf_printk("NO CONTEXT FOUND\n");
+        return;
+    }
+
+    u64 size = context->size;
+    struct task_ctx *ti = bpf_map_lookup_percpu_elem(task_map, &size, cpu_id);
+    if(!ti){
+        bpf_printk("to large of size\n");
+        return;
+    }
+
+    bpf_spin_lock(&context->lock);
+    if(context->size != size){
+        bpf_printk("RACE CONDITION: TASK LOST \n");
+        return;
+    }
+    ti->vruntime = vtime;
+    ti->pid = pid;
+    ti->valid = valid;
+
+    bpf_spin_unlock(&context->unlock);
 }
 
+static long sample_cb(u64 idx, struct random_sample_ctx *rand_cxt)
+{
+    struct random_sample_ctx *s = (struct random_sample_ctx *)rand_cxt;
+
+    // use bpf_get_prandom_u32() inside callback
+    u32 r = bpf_get_prandom_u32();
+    bpf_spin_lock(&map_lock);
+    u32 key = r % map_size;
+    bpf_spin_unlock(&map_lock);
+    struct task_ctx *ti = bpf_map_lookup_elem(&task_map, &key);
+    if (!ti || !ti->valid) return 0; // continue
+
+    if (ti->vruntime < s->best_vtime) {
+        s->best_vtime = ti->vruntime;
+        s->best_key = key;
+    }
+
+    // Optional early exit if time exceeded:
+    if (bpf_ktime_get_ns() - s->start_ns >= s->window_ns || idx >= map_size){
+        //bpf_printk("Exited because of time: %llu\n", idx);
+        return 1; // bpf_loop will stop early if callback returns 1
+    }
+
+    // may want to also add logic to cancel if you have sampled more than the size map_size
+    // currently with the static window of 500ns it samples around 15 (on my pc)
+    // this is overkill if the map_size is small, which it often will be, although maybe not
+    // for benchmarks
+        
+    return 0; // continue
+}
 
 void BPF_STRUCT_OPS(rand2_dispatch, s32 cpu, struct task_struct *prev)
 {
 
-    int res = scx_bpf_random_sample(task_map, map_info, SAMPLE_WINDOW_NS);
-    if(res == -1){
-        return;
-    }
-    struct task_struct *task = bpf_task_from_pid(pid);
-    if (!task){
-        //bpf_printk("task struct null\n");
-        return;
-    }
-    scx_bpf_dsq_insert(task, SHARED_DSQ, SCX_SLICE_DFL, 0);
-    bpf_task_release(task);
-    //bpf_printk("Successful Dispatch\n");
-    stat_inc(2);
-    scx_bpf_dsq_move_to_local(SHARED_DSQ);
+
 }
 
 void BPF_STRUCT_OPS(rand2_running, struct task_struct *p)
